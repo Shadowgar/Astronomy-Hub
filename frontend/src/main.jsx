4a9416e28e862868818a228a3db32947fdbbba7b
ShadowGar <rocco.paul@gmail.com>
2026-03-24 18:44:46 -0400

    feat(design): wire design tokens into app (B1.B4)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import "./design/tokens.css";
+import "./design/themes.css";
+import "./design/semantic.css";
 import './styles.css'
 
 const root = createRoot(document.getElementById('root'))
